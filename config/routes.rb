68a32659a12008ae013fd937c08942b831768f18
Dan Garro <dobetterwithdan@gmail.com>
2022-12-14 21:08:49 -0500

    second commit: adding css

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "reports#index" 
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/reports", to: "reports#index" 
+  get "/reports/:id", to: "reports#show" 
+
 end
---
+++ db/migrate/20221215014639_create_reports.rb
@@ -0,0 +1,11 @@
+class CreateReports < ActiveRecord::Migration[7.0]
+  def change
+    create_table :reports do |t|
+      t.string :title
+      t.text :body
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
